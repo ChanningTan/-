05039a716d15b33b4415e67591a8dde1faef75e0
ChanningTan <tcg1991@live.cn>
2016-12-22 21:42:58 +0800

    37

---
+++ Task-37 弹窗组件/Task37.js
@@ -0,0 +1,115 @@
+/**
+ * Created by Channing on 2016/12/22.
+ */
+
+var EventUtil = {
+    addHandler: function(element, type, handler){
+        if (element.addEventListener){
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent){
+            element.attachEvent("on"+type, handler);
+        } else {
+            element["on"+type] = handler;
+        }
+    },
+    removeHandler: function(element, type, handler){
+        if (element.removeEventListener){
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent){
+            element.detachEvent("on"+type, handler);
+        } else{
+            element["on"+type] = null;
+        }
+    }
+};
+
+function FloatWindow(floatWindowObject, ShowElement, width, height) {
+    this.body = document.createElement('DIV');
+    this.body.className = 'float-window hidden';
+    this.setWindow(floatWindowObject, width, height);
+    this.setWindowShow(ShowElement);
+    this.setWindowHidden(this.body);
+    this.setWindowHidden(document.querySelector('.float-window-nav i'));
+    this.setWindowHidden(document.querySelector('.cancel'));
+}
+FloatWindow.prototype = {
+    constructor: FloatWindow,
+
+    noScrollHandler: function () {
+        event.preventDefault();
+    },
+
+    //根据对象内容创建弹窗
+    setWindow: function (floatWindowObject, width, height) {
+        this.body.innerHTML = floatWindowObject.windowHTML;
+        this.body.querySelector('.float-window-content').innerHTML = floatWindowObject.contentHTML;
+        document.body.insertBefore(this.body, document.body.firstChild);
+
+        var a = width || 480,
+            b = height || 400;
+        this.body.querySelector('.float-window-main').style.width = a + 'px';
+        this.body.querySelector('.float-window-main').style.height = b + 'px';
+    },
+
+    //显示弹窗按钮的事件
+    setWindowShow: function (showElement) {
+        var self = this;
+         EventUtil.addHandler(showElement, 'click', function () {
+             self.body.classList.remove('hidden');
+             self.body.style.top = document.body.scrollTop + 'px';
+             self.body.style.left = document.body.scrollLeft + 'px';
+             //禁止页面滚动
+             EventUtil.addHandler(document, 'mousewheel', self.noScrollHandler);
+         });
+    },
+
+    //关闭弹窗事件
+    setWindowHidden: function (hiddenElement) {
+        var self = this;
+        EventUtil.addHandler(hiddenElement, 'click', function (event) {
+            if (event.target == hiddenElement){
+                self.body.classList.add('hidden');
+                EventUtil.removeHandler(document, 'mousewheel', self.noScrollHandler);
+            }
+        });
+    }
+};
+
+
+//========= main ====================================
+
+//浮窗大小的参数在css中做了限制，width{480, 1000} height{400, 800}
+var floatWindow = new FloatWindow(floatWindowObject, document.querySelector('#floatWindow'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task-37 弹窗组件/floatWindowObject.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Channing on 2016/12/22.
+ */
+
+var floatWindowObject = {
+
+    windowHTML:
+        '<div class="float-window-main">\
+            <div class="float-window-nav"><i>x</i></div>\
+            <p class="float-window-tittle">弹窗的标题</p>\
+            <div class="float-window-content"></div>\
+            <div class="float-window-button">\
+                <button>确定</button>\
+                <button class="cancel">取消</button>\
+            </div>\
+        </div>',
+
+    contentHTML: '这里是弹窗内容HTML',
+
+    cssText: ''
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
